da75fa258df9bce64cfeb69cf130e9454449e70b
kapilkhub <31470043+kapilkhub@users.noreply.github.com>
2024-04-13 15:46:05 +0200

    removed string hardcoding

--- GildedRose/Program.cs
+++ GildedRose/Program.cs
@@ -12,31 +12,31 @@ namespace GildedRoseKata
             Console.WriteLine("OMGHAI!");
 
                 Items = new List<Item>{
-                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
+                new Item {Name = RoseName.Dexterity, SellIn = 10, Quality = 20},
+                new Item {Name = RoseName.AgedBrie, SellIn = 2, Quality = 0},
+                new Item {Name = RoseName.Mongoose, SellIn = 5, Quality = 7},
+                new Item {Name = RoseName.Sulfuras, SellIn = 0, Quality = 80},
+                new Item {Name = RoseName.Sulfuras, SellIn = -1, Quality = 80},
                 new Item
                 {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    Name = RoseName.BackstagePasses,
                     SellIn = 15,
                     Quality = 20
                 },
                 new Item
                 {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    Name = RoseName.BackstagePasses,
                     SellIn = 10,
                     Quality = 49
                 },
                 new Item
                 {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    Name = RoseName.BackstagePasses,
                     SellIn = 5,
                     Quality = 49
                 },
 				// this conjured item does not work properly yet
-				new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+				new Item {Name =RoseName.Conjured, SellIn = 3, Quality = 6}
             };
 
             var app = new GildedRose(Items);
--- GildedRose/RoseName.cs
+++ GildedRose/RoseName.cs
@@ -5,5 +5,8 @@
 		public const string Sulfuras = "Sulfuras, Hand of Ragnaros";
 		public const string BackstagePasses = "Backstage passes to a TAFKAL80ETC concert";
 		public const string AgedBrie = "Aged Brie";
+		public const string Dexterity = "+5 Dexterity Vest";
+		public const string Mongoose = "Elixir of the Mongoose";
+		public const string Conjured = "Conjured Mana Cake";
 	}
 }
