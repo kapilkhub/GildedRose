f6d476d172e4da9ed55db034755a92af2792dab5
kapilkhub <31470043+kapilkhub@users.noreply.github.com>
2024-04-13 13:18:27 +0200

    commit for normal quality constraints

--- GildedRose/Program.cs
+++ GildedRose/Program.cs
@@ -5,11 +5,13 @@ namespace GildedRoseKata
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static IList<Item> Items;
+
+		public static void Main(string[] args)
         {
             Console.WriteLine("OMGHAI!");
 
-            IList<Item> Items = new List<Item>{
+                Items = new List<Item>{
                 new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
                 new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
                 new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
--- GildedRoseTests/QualityTest.cs
+++ GildedRoseTests/QualityTest.cs
@@ -1,19 +1,35 @@
 ﻿using GildedRoseKata;
+using System.Linq;
 using Xunit;
 
 namespace GildedRoseTests
 {
 	public class QualityTest
 	{
-        public QualityTest()
-        {
+		public QualityTest()
+		{
 			Program.Main(new string[] { "30" });
 		}
-      
+
 		[Fact]
-		public void quality_of_items_cannot_be_negative() 
+		public void quality_of_items_cannot_be_negative()
 		{
-			Program.Items
+			var negativeQuality = Program.Items.Any(item => item.Quality < 0);
+			Assert.False(negativeQuality);
+		}
+
+		[Fact]
+		public void quality_of_items_cannot_be_more_than_50()
+		{
+			var quality = Program.Items.Where(item => !item.Name.StartsWith("Sulfuras")).Any(item => item.Quality > 50);
+			Assert.False(quality);
+		}
+
+		[Fact]
+		public void quality_of_Sulfuras_should_always_be_80()
+		{
+			var quality = Program.Items.Any(item => item.Name.StartsWith("Sulfuras") && item.Quality!=80);
+			Assert.False(quality);
 		}
 	}
 }
