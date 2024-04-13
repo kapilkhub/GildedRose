e70b35396576148778862db5d7d93b9b859f76b5
kapilkhub <31470043+kapilkhub@users.noreply.github.com>
2024-04-13 13:03:20 +0200

    commit for git ignore

---
+++ GildedRoseTests/QualityTest.cs
@@ -0,0 +1,19 @@
+﻿using GildedRoseKata;
+using Xunit;
+
+namespace GildedRoseTests
+{
+	public class QualityTest
+	{
+        public QualityTest()
+        {
+			Program.Main(new string[] { "30" });
+		}
+      
+		[Fact]
+		public void quality_of_items_cannot_be_negative() 
+		{
+			Program.Items
+		}
+	}
+}
