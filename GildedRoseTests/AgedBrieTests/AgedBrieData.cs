d9fa5c0aae56714f65aee25bb5a5498ef4d5deb6
kapilkhub <31470043+kapilkhub@users.noreply.github.com>
2024-04-13 14:10:40 +0200

    commit foe AgedBrieData

---
+++ GildedRoseTests/AgedBrieTests/AgedBrieData.cs
@@ -0,0 +1,21 @@
+﻿using GildedRoseKata;
+using Xunit;
+
+namespace GildedRoseTests.AgedBrieTests
+{
+	internal class AgedBrieData : TheoryData<AgedBrie>
+	{
+		public AgedBrieData()
+		{
+			Add(new AgedBrie() { Item = new Item { Name = "Aged Brie", Quality = 10, SellIn = 15 }, ExpectedQuality = 11, ExpectedSellIn = 14 });
+			Add(new AgedBrie() { Item = new Item { Name = "Aged Brie", Quality = 50, SellIn = 7 }, ExpectedQuality = 50, ExpectedSellIn = 6 });
+		}
+	}
+
+	public class AgedBrie
+	{
+		public Item Item { get; set; }
+		public int ExpectedQuality { get; set; }
+		public int ExpectedSellIn { get; set; }
+	}
+}
---
+++ GildedRoseTests/AgedBrieTests/AgedBrieTest.cs
@@ -0,0 +1,38 @@
+﻿using GildedRoseKata;
+using System.Collections.Generic;
+using Xunit;
+
+namespace GildedRoseTests.AgedBrieTests
+{
+	public class AgedBrieTest
+    {
+		/// <summary>
+		/// "Aged Brie" actually increases in Quality the older it gets
+		/// "Aged Brie" quality does not exceed 50
+		/// </summary>
+		/// <param name="data"></param>
+		[Theory]
+        [ClassData(typeof(AgedBrieData))]
+        public void aged_brie_quality_increases_as_older_it_gets(AgedBrie data)
+        {
+            //Arange 
+            var Items = new List<Item>
+            {
+                data.Item
+            };
+            GildedRoseStub _gildedRose = new GildedRoseStub(Items);
+
+            //Act
+            _gildedRose.UpdateQuality();
+
+            //Assert Quality
+            Assert.Equal(data.ExpectedQuality, _gildedRose.Items[0].Quality);
+            
+            //Assert sell in
+            Assert.Equal(data.ExpectedSellIn, _gildedRose.Items[0].SellIn);
+			
+		}
+    }
+
+
+}
