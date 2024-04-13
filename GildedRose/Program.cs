6a984e08fc0433994b6920066991964fa0be1b85
kapilkhub <31470043+kapilkhub@users.noreply.github.com>
2024-04-13 15:47:19 +0200

    commit for new Item to new()

--- GildedRose/Program.cs
+++ GildedRose/Program.cs
@@ -3,56 +3,56 @@ using System.Collections.Generic;
 
 namespace GildedRoseKata
 {
-    public class Program
-    {
-        public static IList<Item> Items;
+	public class Program
+	{
+		public static IList<Item> Items;
 
 		public static void Main(string[] args)
-        {
-            Console.WriteLine("OMGHAI!");
+		{
+			Console.WriteLine("OMGHAI!");
 
-                Items = new List<Item>{
-                new Item {Name = RoseName.Dexterity, SellIn = 10, Quality = 20},
-                new Item {Name = RoseName.AgedBrie, SellIn = 2, Quality = 0},
-                new Item {Name = RoseName.Mongoose, SellIn = 5, Quality = 7},
-                new Item {Name = RoseName.Sulfuras, SellIn = 0, Quality = 80},
-                new Item {Name = RoseName.Sulfuras, SellIn = -1, Quality = 80},
-                new Item
-                {
-                    Name = RoseName.BackstagePasses,
-                    SellIn = 15,
-                    Quality = 20
-                },
-                new Item
-                {
-                    Name = RoseName.BackstagePasses,
-                    SellIn = 10,
-                    Quality = 49
-                },
-                new Item
-                {
-                    Name = RoseName.BackstagePasses,
-                    SellIn = 5,
-                    Quality = 49
-                },
+			Items = new List<Item>{
+				new() {Name = RoseName.Dexterity, SellIn = 10, Quality = 20},
+				new() {Name = RoseName.AgedBrie, SellIn = 2, Quality = 0},
+				new() {Name = RoseName.Mongoose, SellIn = 5, Quality = 7},
+				new() {Name = RoseName.Sulfuras, SellIn = 0, Quality = 80},
+				new() {Name = RoseName.Sulfuras, SellIn = -1, Quality = 80},
+				new()
+				{
+					Name = RoseName.BackstagePasses,
+					SellIn = 15,
+					Quality = 20
+				},
+				new ()
+				{
+					Name = RoseName.BackstagePasses,
+					SellIn = 10,
+					Quality = 49
+				},
+				new ()
+				{
+					Name = RoseName.BackstagePasses,
+					SellIn = 5,
+					Quality = 49
+				},
 				// this conjured item does not work properly yet
-				new Item {Name =RoseName.Conjured, SellIn = 3, Quality = 6}
-            };
+				new () {Name =RoseName.Conjured, SellIn = 3, Quality = 6}
+			};
 
-            var app = new GildedRose(Items);
+			var app = new GildedRose(Items);
 
 
-            for (var i = 0; i < 31; i++)
-            {
-                Console.WriteLine("-------- day " + i + " --------");
-                Console.WriteLine("name, sellIn, quality");
-                for (var j = 0; j < Items.Count; j++)
-                {
-                    System.Console.WriteLine(Items[j].Name + ", " + Items[j].SellIn + ", " + Items[j].Quality);
-                }
-                Console.WriteLine("");
-                app.UpdateQuality();
-            }
-        }
-    }
+			for (var i = 0; i < 31; i++)
+			{
+				Console.WriteLine("-------- day " + i + " --------");
+				Console.WriteLine("name, sellIn, quality");
+				for (var j = 0; j < Items.Count; j++)
+				{
+					System.Console.WriteLine(Items[j].Name + ", " + Items[j].SellIn + ", " + Items[j].Quality);
+				}
+				Console.WriteLine("");
+				app.UpdateQuality();
+			}
+		}
+	}
 }
